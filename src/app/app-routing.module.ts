a51922c4722e5dfdc7ab08c1aa2311979354ac25
khaledchtioui <80386326+khaledchtioui@users.noreply.github.com>
2024-04-20 17:12:36 +0100

    20/04/24

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,63 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { BaseFrontComponent } from './base-front/base-front.component';
+import {Error404Component} from "./errors/error404/error404.component";
+import {Error500Component} from "./errors/error500/error500.component";
+import {ArticlefrontComponent} from "./article/articlefront/articlefront.component";
+import {ArticlelistComponent} from "./article/articlelist/articlelist.component";
+import {ArticlegridComponent} from "./article/articlegrid/articlegrid.component";
+import {ArticledetailsComponent} from "./article/articledetails/articledetails.component";
+import {LoginComponent} from "./user/login/login.component";
+import {ForgotpasswordComponent} from "./user/forgotpassword/forgotpassword.component";
+import {SettingeditprofileComponent} from "./user/settingeditprofile/settingeditprofile.component";
+import {TermsconditionsComponent} from "./user/termsconditions/termsconditions.component";
+import {PrivacyPolicyComponent} from "./user/privacy-policy/privacy-policy.component";
+import {SigninComponent} from "./user/signin/signin.component";
+import {DashboardComponent} from "./backoffice/student/dashboard/dashboard.component";
+import {EditprofileComponent} from "./backoffice/student/editprofile/editprofile.component";
+import {DeleteprofileComponent} from "./backoffice/student/deleteprofile/deleteprofile.component";
+import {MyarticleComponent} from "./backoffice/student/myarticle/myarticle.component";
+import {DashboardAdminComponent} from "./backoffice/admin/dashboard-admin/dashboard-admin.component";
+import {UserlistComponent} from "./backoffice/admin/userlist/userlist.component";
+import {UsergridComponent} from "./backoffice/admin/usergrid/usergrid.component";
+import {AllarticleComponent} from "./backoffice/admin/allarticle/allarticle.component";
 
-import { FullComponent } from './layouts/full/full.component';
+const appRoutes: Routes = [
+  {path:"" ,component:BaseFrontComponent},
+  {path:"front" ,component:BaseFrontComponent},
+  {path:"login",component:LoginComponent},
+  {path:"forgotpassword",component:ForgotpasswordComponent},
+  {path:"editprofile",component:SettingeditprofileComponent},
+  {path:"register",component:SigninComponent},
+  {path:"article",component:ArticlefrontComponent},
+  {path:"termsconditions",component:TermsconditionsComponent},
+  {path:"privacypolicy",component:PrivacyPolicyComponent},
+  {path:"forgotpassword",component:ForgotpasswordComponent},
+  {path:"articlelist",component:ArticlelistComponent},
+  {path:"articlegrid",component:ArticlegridComponent},
+  {path:"articledetails",component:ArticledetailsComponent},
+  {path:"error404",component:Error404Component},
+  {path:"error500",component:Error500Component},
+  {path:"studentdahboard",component:DashboardComponent},
+  {path:"admindahboard",component:DashboardAdminComponent},
+  {path:"adminusers",component:UserlistComponent},
+  {path:"adminusersgrid",component:UsergridComponent},
+  {path:"studentedit",component:EditprofileComponent},
+  {path:"studentdelete",component:DeleteprofileComponent},
+  {path:"studentmyarticle",component:MyarticleComponent},
+  {path:"adminallarticle",component:AllarticleComponent},
 
-export const Approutes: Routes = [
-  {
-    path: '',
-    component: FullComponent,
-    children: [
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-      {
-        path: 'dashboard',
-        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
-      },
-      {
-        path: 'about',
-        loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
-      },
-      {
-        path: 'component',
-        loadChildren: () => import('./component/component.module').then(m => m.ComponentsModule)
-      }
-    ]
-  },
-  {
-    path: '**',
-    redirectTo: '/starter'
-  }
+
+
+
 ];
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+
+
+
+export class AppRoutingModule { }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,74 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  CommonModule, LocationStrategy,
-  PathLocationStrategy
-} from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { Routes, RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-import { FullComponent } from './layouts/full/full.component';
-
-
-import { NavigationComponent } from './shared/header/navigation.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-
-import { Approutes } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SpinnerComponent } from './shared/spinner.component';
-
+import { BaseFrontComponent } from './base-front/base-front.component';
+import { LoginComponent } from './user/login/login.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { ForgotpasswordComponent } from './user/forgotpassword/forgotpassword.component';
+import { SettingeditprofileComponent } from './user/settingeditprofile/settingeditprofile.component';
+import {NgOptimizedImage} from "@angular/common";
+import { ArticlefrontComponent } from './article/articlefront/articlefront.component';
+import { Navbar2Component } from './shared/navbar2/navbar2.component';
+import { TermsconditionsComponent } from './user/termsconditions/termsconditions.component';
+import { PrivacyPolicyComponent } from './user/privacy-policy/privacy-policy.component';
+import { ArticlelistComponent } from './article/articlelist/articlelist.component';
+import { ArticlegridComponent } from './article/articlegrid/articlegrid.component';
+import { ArticledetailsComponent } from './article/articledetails/articledetails.component';
+import { Error404Component } from './errors/error404/error404.component';
+import { Error500Component } from './errors/error500/error500.component';
+import { SigninComponent } from './user/signin/signin.component';
+import {FooterComponent} from "./shared/footer/footer.component";
+import { DashboardComponent } from './backoffice/student/dashboard/dashboard.component';
+import { Navbar3Component } from './shared/navbar3/navbar3.component';
+import { Sidebar1Component } from './shared/sidebar1/sidebar1.component';
+import { EditprofileComponent } from './backoffice/student/editprofile/editprofile.component';
+import { DeleteprofileComponent } from './backoffice/student/deleteprofile/deleteprofile.component';
+import { MyarticleComponent } from './backoffice/student/myarticle/myarticle.component';
+import { Sidebar2Component } from './shared/sidebar2/sidebar2.component';
+import { DashboardAdminComponent } from './backoffice/admin/dashboard-admin/dashboard-admin.component';
+import { UserlistComponent } from './backoffice/admin/userlist/userlist.component';
+import { UsergridComponent } from './backoffice/admin/usergrid/usergrid.component';
+import { AllarticleComponent } from './backoffice/admin/allarticle/allarticle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SpinnerComponent,
+    BaseFrontComponent,
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent,
+    ForgotpasswordComponent,
+    SettingeditprofileComponent,
+    ArticlefrontComponent,
+    Navbar2Component,
+    TermsconditionsComponent,
+    PrivacyPolicyComponent,
+    ArticlelistComponent,
+    ArticlegridComponent,
+    ArticledetailsComponent,
+    Error404Component,
+    Error500Component,
+    SigninComponent,
+    DashboardComponent,
+    Navbar3Component,
+    Sidebar1Component,
+    EditprofileComponent,
+    DeleteprofileComponent,
+    MyarticleComponent,
+    Sidebar2Component,
+    DashboardAdminComponent,
+    UserlistComponent,
+    UsergridComponent,
+    AllarticleComponent
   ],
   imports: [
-    CommonModule,
     BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    NgbModule,
-    RouterModule.forRoot(Approutes, { useHash: false }),
-    FullComponent,
-    NavigationComponent,
-    SidebarComponent,
+    AppRoutingModule,
+    NgOptimizedImage,
   ],
-  providers: [
-    {
-      provide: LocationStrategy,
-      useClass: PathLocationStrategy
-    },
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
