7c2c065d9dc6429dcb87f019b7d3808c3d6df90c
khaledchtioui <80386326+khaledchtioui@users.noreply.github.com>
2024-04-20 17:12:36 +0100

    20/04/24

---
+++ src/app/article/articledetails/articledetails.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-articledetails',
+  templateUrl: './articledetails.component.html',
+  styleUrls: ['./articledetails.component.css']
+})
+export class ArticledetailsComponent {
+
+}
---
+++ src/app/backoffice/admin/allarticle/allarticle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-allarticle',
+  templateUrl: './allarticle.component.html',
+  styleUrls: ['./allarticle.component.css']
+})
+export class AllarticleComponent {
+
+}
