4b5993d23888bd0fd12aaf7c381e12b6238ac5a3
Roua411 <roua.chiboub@esprit.tn>
2024-04-22 21:35:36 +0100

    getallarticles

--- src/app/backoffice/admin/allarticle/allarticle.component.ts
+++ src/app/backoffice/admin/allarticle/allarticle.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Article } from 'src/app/models/Article';
+import { ArticleService } from 'src/app/service/Article/article.service';
 
 @Component({
   selector: 'app-allarticle',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./allarticle.component.css']
 })
 export class AllarticleComponent {
+  
+  articles: Article[] = [];
 
+  constructor(private articleService: ArticleService) { }
+
+  ngOnInit(): void {
+    this.loadArticles();
+  }
+
+  loadArticles(): void {
+    this.articleService.getAllArticles().subscribe(
+      articles => {
+        this.articles = articles;
+      },
+      error => {
+        console.log('Une erreur s\'est produite lors du chargement des articles : ', error);
+      }
+    );
+
+}
 }
---
+++ src/app/service/Article/article.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Article } from 'src/app/models/Article';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+
+  
+  private baseUrl = 'http://localhost:8080/Articles'; // URL de base pour votre API Spring
+
+  constructor(private http: HttpClient) { }
+
+  getAllArticles(): Observable<Article[]> {
+    return this.http.get<Article[]>(this.baseUrl);
+  }
+
+
+}
