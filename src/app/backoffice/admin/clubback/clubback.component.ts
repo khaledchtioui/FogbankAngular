d979c7f4ef7fe340b6ed72ec88ef8d0af4c8dc7c
unknown <omar.benamara@edu.isetcom.tn>
2024-05-05 21:47:58 +0200

    Filter

--- src/app/backoffice/admin/clubback/clubback.component.ts
+++ src/app/backoffice/admin/clubback/clubback.component.ts
@@ -14,12 +14,17 @@ import { ClubService } from '../../../service/Club/club.service';
 
 export class ClubbackComponent implements OnInit{
   clubs: Club[] = [];
+  selectedCategory: string = 'All Clubs';
+  originalClubs!: Club[];
+
   constructor(private clubService: ClubService,private router: Router) { }
 
   ngOnInit(): void {
     this.retrieveClubs();
     
   }
+
+
   navigateToAddClub() {
     this.router.navigate(['/admin/club/add']);
   }
@@ -36,6 +41,21 @@ export class ClubbackComponent implements OnInit{
         console.error('Error fetching clubs:', error);
       }
     );
+  }
+
+  filterClubsByCategory(): void {
+    if (!this.originalClubs) {
+      // Initialize originalClubs with the current clubs array
+      this.originalClubs = [...this.clubs];
+    }
+    
+    if (this.selectedCategory === 'All Clubs') {
+      // If 'Choose' is selected, show all clubs from the original list
+      this.clubs = [...this.originalClubs];
+    } else {
+      // Filter clubs based on selected category from the original list
+      this.clubs = this.originalClubs.filter(club => club.cat === this.selectedCategory);
+    }
   }
 
 }
